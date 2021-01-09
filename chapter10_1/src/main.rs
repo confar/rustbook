0358f3a57f3eaf979449ef23f1d2568796e8dd25
confar <tentigers@mail.ru>
2021-01-09 23:52:49 +0300

    refactor minigrep

--- chapter10_1/src/main.rs
+++ chapter10_1/src/main.rs
@@ -1,22 +1,22 @@
 
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
 
-    for &item in list.iter() {
+    for item in list.iter() {
         if item > largest {
             largest = item;
         }
     }
-    largest
+    &largest
 }
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest(&number_list);
+    let result = largest(number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let number_list = vec!["y", "m", "c", "a"];
 
     let result = largest(&number_list);
     println!("The largest number is {}", result);
