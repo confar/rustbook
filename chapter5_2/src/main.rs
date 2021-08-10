f350da9d428267e0b2bd51631e0e56acdb3cb9df
Alex Galkin <tentigers@mail.ru>
2021-08-10 23:57:31 +0300

    Update main.rs

--- chapter5_2/src/main.rs
+++ chapter5_2/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     let rect2 = Rectangle { width: 10, height: 40 };
     let rect3 = Rectangle { width: 60, height: 45 };
 
-
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 
