e18ba7f3794f1665f0ad41fd44661f7863dfc87d
Alex Galkin <tentigers@mail.ru>
2021-09-13 23:56:34 +0300

    Update main.rs

--- chapter6_1/src/main.rs
+++ chapter6_1/src/main.rs
@@ -15,6 +15,7 @@ enum Message {
 fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
     let loopback = IpAddr::V6(String::from("::1"));
+    
     println!("{:?}", loopback);
     let some_number = Some(5);
     let some_string = Some("a string");
